770b78cb07f4abbc80b3eb4c979efa0caf8dec45
Darin Carrigg <d-carrigg@users.noreply.github.com>
2024-01-17 14:44:14 -0600

    Cleaned up runtime script

--- src/Modules/BinWips/files/Setup-Runtime.ps1
+++ src/Modules/BinWips/files/Setup-Runtime.ps1
@@ -1,45 +1,42 @@
-﻿function Get-BinWipsResource {
-   [Alias("Get-PSBinaryResource")]
-    [cmdletbinding()]
-    param (
-       [Parameter(Mandatory=$true,Position=0)]
-       [string] $Path,
- 
-       [Parameter(Mandatory=$false,Position=1)]
-       [switch]$AsFile
-    )
-   $csharp = @"
-   using System.IO;
-   using System.IO.Pipes;
-   using System;
-   
-   namespace BinWips.Runtime
-   {
-       public static class RuntimeUtils
-       {
-           public static string GetResource(string path)
-           {
-               var client = new NamedPipeClientStream("BinWipsPipe{#BinWipsPipeGuid#}");
-               client.Connect();
-                
-               StreamReader reader = new StreamReader(client);
-               StreamWriter writer = new StreamWriter(client);
-   
-               string input = path;
-               writer.WriteLine(input);
-               writer.Flush();
-               var result = reader.ReadLine();
-               return result;
-           }
-       }
-   }
+﻿$csharp = @"
+using System.IO;
+using System.IO.Pipes;
+using System;
+
+namespace BinWips
+{
+    public static class RuntimeUtils
+    {
+        public static string GetResource(string path)
+        {
+            var client = new NamedPipeClientStream("BinWipsPipe{#BinWipsPipeGuid#}");
+            client.Connect();
+             
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+
+            string input = path;
+            writer.WriteLine(input);
+            writer.Flush();
+            var result = reader.ReadLine();
+            return result;
+        }
+    }
+}
 "@
 
-    Add-Type $csharp
-    [BinWips.Runtime.RuntimeUtils]::GetResource($Path)
-   
- }     
+Add-Type $csharp
 
-#  function Write-Output {
-#    param($InputObject) process {Write-Host $InputObject}
-#  }
+function Get-BinWipsResource
+{
+   [Alias("Get-PSBinaryResource")]
+   [cmdletbinding()]
+   param (
+      [Parameter(Mandatory = $true, Position = 0)]
+      [string] $Path
+   )
+    
+   [BinWips.RuntimeUtils]::GetResource($Path)
+   
+}     
+ 
